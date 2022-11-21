d3bad44ec9ce6841e5e4bea88c63c6467b107aec
icaamk <114427475+icaamk@users.noreply.github.com>
2022-11-21 17:21:45 +0700

    Add files via upload

---
+++ PBO-OperasiCRUD/pbotabel.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2022 at 11:05 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.0.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pbotabel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_jk`
+--
+
+CREATE TABLE `tbl_jk` (
+  `id_jk` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_jk`
+--
+
+INSERT INTO `tbl_jk` (`id_jk`, `nama`) VALUES
+(1, 'Laki-Laki'),
+(2, 'Perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mhs`
+--
+
+CREATE TABLE `tbl_mhs` (
+  `id_mhs` int(11) NOT NULL,
+  `nim` varchar(255) NOT NULL,
+  `nama_mhs` varchar(255) NOT NULL,
+  `jk` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `prodi` int(11) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_mhs`
+--
+
+INSERT INTO `tbl_mhs` (`id_mhs`, `nim`, `nama_mhs`, `jk`, `alamat`, `prodi`, `foto`, `email`) VALUES
+(1, '21051214012', 'Ica Amilatul K', 2, 'Surabaya', 1, 'Ica Amilatul Kholidah_21051214012_Tugas1.docx', 'icaamilatul.21012@mhs.unesa.ac.id'),
+(2, '21051214065', 'Valen Zidana', 2, 'Surabaya', 2, 'LAPORAN AKHIR PENGANTAR BISNIS DAN MANAJEMEN.docx', 'valenzidana@mhs.unesa.ac.id'),
+(3, '21051214001', 'Alfina Fifi', 1, 'Jombang', 1, 'Progres 2.docx', 'alfinafikko@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_prodi`
+--
+
+CREATE TABLE `tbl_prodi` (
+  `id_prodi` int(11) NOT NULL,
+  `jenjang` varchar(255) NOT NULL,
+  `nama_prodi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_prodi`
+--
+
+INSERT INTO `tbl_prodi` (`id_prodi`, `jenjang`, `nama_prodi`) VALUES
+(1, 'S1', 'Sistem Informasi'),
+(2, 'S1', 'Teknik Informatika'),
+(3, 'S1', 'Pendidikan Teknologi Informasi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_jk`
+--
+ALTER TABLE `tbl_jk`
+  ADD PRIMARY KEY (`id_jk`);
+
+--
+-- Indexes for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  ADD PRIMARY KEY (`id_mhs`);
+
+--
+-- Indexes for table `tbl_prodi`
+--
+ALTER TABLE `tbl_prodi`
+  ADD PRIMARY KEY (`id_prodi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_jk`
+--
+ALTER TABLE `tbl_jk`
+  MODIFY `id_jk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `tbl_prodi`
+--
+ALTER TABLE `tbl_prodi`
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
